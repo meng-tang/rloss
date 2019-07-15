4ebbd0e6dc771fd7c3ce0f156c3949b15fc147a8
Meng Tang <mengtang@mengtang-fedora-it2277872.DHCP.thefacebook.com>
2019-07-15 14:41:32 -0700

    scribbles data

--- data/pascal_scribble/fetchPascalScribble.sh
+++ data/pascal_scribble/fetchPascalScribble.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-# download scribble annotations from http://www.jifengdai.org/downloads/scribble_sup/
+# download scribble annotations from http://cs.uwaterloo.ca/~m62tang/rloss/
+# annotation is given in the format of pixelwise labeling
+if [ ! -f "pascal_2012_scribble.zip" ];
+then
+	wget http://cs.uwaterloo.ca/~m62tang/rloss/pascal_2012_scribble.zip
+	unzip pascal_2012_scribble.zip
+fi
+
+# Alternatively, scribbles can be obtained from http://www.jifengdai.org/downloads/scribble_sup/
+# which is in the format of XY coordinates of scribble skeleton.
+# Then obtain pixel-wise labeling by running convertscribbles.m
 if [ ! -f "scribble_annotation.zip" ];
 then
 	wget https://www.dropbox.com/s/9vh3kvtd742red8/scribble_annotation.zip --no-check-certificate
